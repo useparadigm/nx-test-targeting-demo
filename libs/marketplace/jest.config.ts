8cc14e92ace08d123ca942822f4af39b24c35870
jsobolew <qba.sobolewski@gmail.com>
2026-04-17 13:09:33 +0200

    chore: add NX + Jest + ts-jest setup for runnable tests

--- libs/marketplace/jest.config.ts
+++ libs/marketplace/jest.config.ts
@@ -2,4 +2,8 @@ export default {
   displayName: "marketplace",
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@org/shared$": "<rootDir>/../../libs/shared/src/index.ts",
+    "^@org/marketplace$": "<rootDir>/src/index.ts",
+  },
 };
--- libs/shared/jest.config.ts
+++ libs/shared/jest.config.ts
@@ -2,4 +2,7 @@ export default {
   displayName: "shared",
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@org/shared$": "<rootDir>/src/index.ts",
+  },
 };
